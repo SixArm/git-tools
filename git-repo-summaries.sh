af1669543424caf2600696404e5678379ebf7aaf
SixArm <sixarm@sixarm.com>
2012-01-10 09:32:01 -0800

    Update license

--- git-repo-summaries.sh
+++ git-repo-summaries.sh
@@ -10,13 +10,14 @@
 #
 # The pipe output is a spreadsheet of tab-separated-value rows,
 # where each row is one repo and has this information:
-#   * the repository name, e.g. "my_foo_bar"
-#   * the first commit's date, i.e. whatever git log shows as first by date, e.g. "2010-01-01"
-#   * the last commit's date, i.e. whatever git log shows as most recent by date, e.g. "2010-12-31"
+#
+#   * the repository name, e.g. "my_foo_bar".
+#   * the first commit's date, i.e. whatever git log shows as first by date, e.g. "2010-01-01".
+#   * the last commit's date, i.e. whatever git log shows as most recent by date, e.g. "2010-12-31".
 #   * the number of files (omitting some directories e.g. Ruby gem auto-generated directories).
-#   * the number of lines in the files 
-#   * the version number from a file 'VERSION" if it exists, e.g. "1.2.3"
-#   * the first line of a file "README*" if it exists, e.g. "My Foo Bar"
+#   * the number of lines in the files.
+#   * the version number; this looks for a file "VERSION*" or a "*-*.deb" package version, e.g. "1.2.3".
+#   * the headline; this looks for a file "README*" and uses the first line e.g. "My Foo Bar".
 
 echo -e "SixArm Repository Summaries"
 echo -e "Files and Lines columns omit some directories, e.g. Ruby gem auto-generated directories \"coverage\" and \"doc\"."
@@ -28,37 +29,43 @@ do
  cd $dir
  echo -e -n "$dir"
 
+ if [ -e .git/config ] 
+ then
+   first_commit_date=`git log --date-order --date=iso --pretty="format:%ad" --reverse | head -1 | sed 's/ .*//'`
+   last_commit_date=`git log --date-order --date=iso --pretty="format:%ad" | head -1 | sed 's/ .*//'` 
+ else
+   first_commit_date=""
+   last_commit_date=""
+ fi
+ echo -e -n "\t$first_commit_date\t$last_commit_date"
+
  file_count=`find ./ -path ./.git -prune -o -path ./coverage -prune -o -path ./doc -prune -o -type f -print | wc -l`
  echo -e -n "\t$file_count"
 
  line_count=`( find ./ -path ./.git -prune -o -path ./coverage -prune -o -path ./doc -prune -o -type f -print0 | xargs -I{} -0 cat "{}" ) | wc -l`
  echo -e -n "\t$line_count"
 
- if [ -e .git/config ] 
+ version=""
+ find=`find VERSION*`
+ if [ $? -eq 0 ]
  then
-   first_commit_date=`git log --date-order --date=iso --pretty="format:%ad" --reverse | head -1 | sed 's/ .*//'`
-   last_commit_date=`git log --date-order --date=iso --pretty="format:%ad" | head -1 | sed 's/ .*//'` 
+   file=`find VERSION* | sort | head -1`
+   version=`head -1 $file` 
  else
-   first_commit_date="?"
-   last_commit_date="?"
- fi
- echo -e -n "\t$first_commit_date\t$last_commit_date"
-
- if [ -e VERSION ] 
- then
-   version=`tail -1 VERSION`
- else
-   version="?"
+   file=`find *.deb | sort | tail -1`
+   if [ $? -eq 0 ]
+   then
+     version=`echo "$file" | sed 's/.*-//; s/\.deb$//'`
+   fi
  fi
  echo -e -n "\t$version"
 
- readme=`find README*`
+ head=""
+ find=`find README*`
  if [ $? -eq 0 ]
  then
-   head=`grep "SixArm" $readme | sed 's/^# //; s/^= //;' | head -1`
- else
-   readme="?"
-   head="?"
+   file=`find README* | sort | head -1`
+   head=`grep "SixArm" $file | sed 's/^# //; s/^= //;' | head -1`
  fi
  echo -e -n "\t$head"
  
